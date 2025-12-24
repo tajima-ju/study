a5a0c3ceb46f47d4fece3387ce8bc3ec99e519ea
tajima-ju <tata03060824@gmail.com>
2025-12-24 22:20:07 +0900

    auto update 2025/12/24 22:20:07.07

--- codes/algorithm.php
+++ codes/algorithm.php
@@ -136,3 +136,20 @@ for ($i = 0; $i < $base_array_size - 1; $i++) { //3回まで行う
         $base_array[$tmp_min_index] = $tmp;
     }
 }
+
+
+//バブルソート
+$bubble_array = [2, 3, 4, 5, 1];
+
+$bubble_array_size = count($bubble_array); //5
+
+$tmp = null;
+for ($j = 1; $j <= $bubble_array_size - 1; $j++) {
+    for ($i = $bubble_array_size - $j; $i > 0; $i--) { //$iは4から1まで1ずつ減っていく
+        if ($bubble_array[$i - 1] > $bubble_array[$i]) { //昇順じゃなかったら入れ替え 5>1なの入れ替え
+            $tmp = $bubble_array[$i - 1];
+            $bubble_array[$i - 1] = $bubble_array[$i];
+            $bubble_array[$i] = $tmp;
+        }
+    }
+}
