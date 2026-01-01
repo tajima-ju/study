674049828ed6db3d758f1f31cd0b686fd65e1d50
tajima-ju <tata03060824@gmail.com>
2026-01-01 22:54:13 +0900

    auto update 2026/01/01 22:54:13.39

--- codes/algorithm.php
+++ codes/algorithm.php
@@ -238,3 +238,28 @@ for ($k = 2; $k <= 100; $k++) {
         continue;
     }
 }
+
+
+//二分探索法
+$array = [2, 4, 7, 8, 13, 46];
+
+
+$low_index = 0;
+$high_index = count($array) - 1;
+$guess_num = 6; //探す数字
+
+$found_flag = false;
+
+while ($low_index <= $high_index) {
+    $mid_index = floor(($low_index + $high_index) / 2);
+    $mid_num = $array[$mid_index];
+    if ($mid_num === $guess_num) {
+        $found_flag = true;
+        break;
+    } elseif ($mid_num > $guess_num) {
+        $high_index = $mid_index - 1;
+    } else {
+        $low_index = $mid_index + 1;
+    }
+}
+echo $found_flag ? "見つかりました" : "見つかりませんでした"; //三項演算子 ? 条件 true時:false時
---
+++ codes/queue.php
@@ -0,0 +1,16 @@
+<?php
+var_dump(class_exists('SplQueue')); // true
+//Standard PHP Library (SPL)    phpではクラスとしてすでに提供されている機能がある
+//SplQueueはキューを実装しているクラス
+
+$queue = new SplQueue(); //インスタンス生成、メソッドを使える状態にする
+
+$queue->enqueue('A');
+$queue->enqueue('B');
+$queue->enqueue('C');
+$queue->enqueue('D');
+
+while (!$queue->isEmpty()) { //キューが空になるまで
+    echo $queue->dequeue(); //ABCD
+}
+//SplQueueクラスにはisEmptyメソッドは存在していない。isEmptyメソッドは親から継承して使っている
