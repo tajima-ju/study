2e4431ba1bf29b67f0967fb04846799b3805da53
tajima-ju <tata03060824@gmail.com>
2026-01-02 23:36:48 +0900

    auto update 2026/01/02 23:36:48.54

--- codes/queue.php
+++ codes/queue.php
@@ -14,3 +14,32 @@ while (!$queue->isEmpty()) { //キューが空になるまで
     echo $queue->dequeue(); //ABCD
 }
 //SplQueueクラスにはisEmptyメソッドは存在していない。isEmptyメソッドは親から継承して使っている
+
+
+//幅優先探索法
+function BFS($array, $value)
+{
+
+    $visited_flag = []; //閉路による無限ループ防止
+    $order = [];
+
+    $queue = new SplQueue();
+
+    $queue->enqueue($value);
+    $visited_flag[$value] = true;
+
+    while (!($queue->isEmpty())) { //キューに入っている間
+        $node = $queue->dequeue(); //キューの先頭$nodeに格納
+        $order[] = $node;
+
+        foreach ($array[$node] ?? [] as $next) { //$array[$node]がnullの場合空配列、それ以外$nextに格納していく 
+            if (!isset($visited_flag[$next])) { //flagのキーが存在しているなら
+                $queue->enqueue($next);
+                $visited_flag[$next] = true;
+            }
+        }
+    }
+    return $order;
+}
+
+print_r(BFS($graph, 'A'));
